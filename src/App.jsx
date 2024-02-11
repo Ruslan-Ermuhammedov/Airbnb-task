269fea6821c5bd6ca598816ee7a27424b9494de3
Ruslan-Ermuhammedov <ermuhammedovruslan18@gmail.com>
2024-02-11 11:09:05 +0500

    AB-edit-main-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home/Home'
+import Detail from './pages/Detail/Detail'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='deteil/:id' element={<Detail/>} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Detail/Detail.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { baseUrl } from '../../constants/constants'
+import { useParams } from 'react-router'
+import { IoSearchSharp } from "react-icons/io5";
+import { FiGlobe } from "react-icons/fi";
+import { TfiAlignJustify } from "react-icons/tfi";
+
+function Detail() {
+
+
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
+export default Detail
